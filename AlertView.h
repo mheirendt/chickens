8dcbd228aa33dc02525b4356a301c15a05b534a6
Michael Heirendt <mheirendt@Michaels-MacBook-Pro.local>
2016-02-13 18:18:00 -0700

    First Commit

--- AlertView.h
+++ AlertView.h
@@ -17,19 +17,14 @@
 
 @interface AlertView : CCNode
 
-// -----------------------------------------------------------------
-// properties
-// -----------------------------------------------------------------
-// methods
-
-//+ (instancetype)node;
-//- (instancetype)init;
 
 + (void)ShowAlert: (NSString*) message onLayer: (CCNode *) layer
          withOpt1: (NSString*) opt1 withOpt1Block: (void(^)())opt1Block
           andOpt2: (NSString*) opt2 withOpt2Block: (void(^)())opt2Block;
++ (void) Ask: (NSString *) question onLayer: (CCNode *) layer yesBlock: (void(^)())yesBlock noBlock: (void(^)())noBlock;
++ (void) Confirm: (NSString *) question onLayer: (CCNode *) layer okBlock: (void(^)())okBlock cancelBlock: (void(^)())cancelBlock;
++ (void) Tell: (NSString *) statement onLayer: (CCNode *) layer okBlock: (void(^)())okBlock;
 
-// -----------------------------------------------------------------
 
 @end
 
